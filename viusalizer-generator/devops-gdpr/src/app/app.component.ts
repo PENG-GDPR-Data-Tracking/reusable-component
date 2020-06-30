cad6e7b65e8e234e2e35732b33d0c949a707effe
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-06-30 09:13:15 +0200

    add api

--- viusalizer-generator/devops-gdpr/src/app/app.component.ts
+++ viusalizer-generator/devops-gdpr/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DefaultService } from './zipkin/api/default.service';
+import { Observable } from 'rxjs';
+import { ListOfTraces } from './zipkin';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devops-gdpr';
+
+  traces: ListOfTraces = []
+
+  constructor(api: DefaultService){
+    api.tracesGet(null, null, null, null,null,1593500850091,900000, 100, "response", true).subscribe(ans => {
+      this.traces = ans.body;
+    })
+  }
 }
--- viusalizer-generator/devops-gdpr/src/app/app.module.ts
+++ viusalizer-generator/devops-gdpr/src/app/app.module.ts
@@ -4,6 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { HttpClientModule } from '@angular/common/http';
+import { Configuration, ConfigurationParameters } from './zipkin/configuration';
+import { ApiModule } from './zipkin';
+
+export function apiConfigFactory(): Configuration  {
+  const params: ConfigurationParameters = {
+
+    // set configuration parameters here.
+  };
+  return new Configuration(params);
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +23,9 @@ import { NgxGraphModule } from '@swimlane/ngx-graph';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxGraphModule
+    NgxGraphModule,
+    ApiModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
