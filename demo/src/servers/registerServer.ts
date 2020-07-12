b5e4ed6c7bc1ac1bcb132fffb464cf811e611567
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-07-13 00:07:23 +0200

    make cors work

--- demo/src/servers/registerServer.ts
+++ demo/src/servers/registerServer.ts
@@ -11,19 +11,19 @@ export const registerServer = (server: Server) => {
   const app = express();
   const staticPath = path.resolve(__dirname, `${server.name}/web`);
   // console.log('staticPath', staticPath)
-  app.use(express.static(staticPath), cors());
+  app.use(cors());
+  app.use(express.static(staticPath));
 
-  server.paths.forEach(path => {
+  server.paths.forEach((path) => {
     app.get(path, (req, res) => {
-
       // respond to the client
       res.send(`Response to path ${path} from ${server.name}`);
 
       // pass the request
-      server.remoteUrls.forEach(url => http.get(url));
+      server.remoteUrls.forEach((url) => http.get(url));
     });
-  })
+  });
 
   app.get('*', (req, res) => res.send(`That's it from ${server.name}.`));
   app.listen(server.port, () => console.log(`http://localhost:${server.port}: ${server.name} started.`));
-}
+};
